4269dab544301cb548462b6100225e2722799cc6
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-06-22 16:47:18 +0200

    LPS-115915 Fix tests

--- modules/apps/content-dashboard/content-dashboard-test/src/testIntegration/java/com/liferay/content/dashboard/web/internal/portlet/test/ContentDashboardAdminPortletTest.java
+++ modules/apps/content-dashboard/content-dashboard-test/src/testIntegration/java/com/liferay/content/dashboard/web/internal/portlet/test/ContentDashboardAdminPortletTest.java
@@ -554,6 +554,9 @@ public class ContentDashboardAdminPortletTest {
 		mockLiferayPortletRenderRequest.setAttribute(
 			WebKeys.THEME_DISPLAY, _getThemeDisplay());
 
+		mockLiferayPortletRenderRequest.setAttribute(
+			WebKeys.USER_ID, _user.getUserId());
+
 		return mockLiferayPortletRenderRequest;
 	}
 
@@ -579,7 +582,7 @@ public class ContentDashboardAdminPortletTest {
 		themeDisplay.setCompany(_company);
 		themeDisplay.setPermissionChecker(
 			PermissionThreadLocal.getPermissionChecker());
-		themeDisplay.setUser(_company.getDefaultUser());
+		themeDisplay.setUser(_user);
 
 		return themeDisplay;
 	}
