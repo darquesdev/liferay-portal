18ce329f6b2c0665524ed0079da049cdd5b6485a
David Arques <david.arques@liferay.com>
2020-07-21 18:35:03 +0200

    LPS-117566 WIP inject the rest of values

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/portlet/action/GetAnalyticsReportsPanelMVCResourceCommand.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/portlet/action/GetAnalyticsReportsPanelMVCResourceCommand.java
@@ -17,9 +17,12 @@ package com.liferay.analytics.reports.web.internal.portlet.action;
 import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItem;
 import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItemTracker;
 import com.liferay.analytics.reports.web.internal.constants.AnalyticsReportsPortletKeys;
+import com.liferay.analytics.reports.web.internal.model.TimeRange;
+import com.liferay.analytics.reports.web.internal.model.TimeSpan;
 import com.liferay.info.display.contributor.InfoDisplayContributor;
 import com.liferay.info.display.contributor.InfoDisplayContributorTracker;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -29,16 +32,24 @@ import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
+import java.time.format.DateTimeFormatter;
+
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
+import javax.portlet.ResourceURL;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -64,6 +75,9 @@ public class GetAnalyticsReportsPanelMVCResourceCommand
 
 		Locale locale = _portal.getLocale(resourceRequest);
 
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, getClass());
+
 		try {
 			String className = ParamUtil.getString(
 				resourceRequest, "className");
@@ -80,6 +94,10 @@ public class GetAnalyticsReportsPanelMVCResourceCommand
 
 			if (analyticsReportsInfoItem != null) {
 				jsonObject.put(
+					"context",
+					_getContext(
+						true, true, 0, resourceResponse, locale, resourceBundle)
+				).put(
 					"props",
 					_getAnalyticsReportsInfoItemObjectOptional(
 						className, classPK
@@ -89,7 +107,8 @@ public class GetAnalyticsReportsPanelMVCResourceCommand
 							analyticsReportsInfoItemObject, locale)
 					).orElse(
 						JSONFactoryUtil.createJSONObject()
-					));
+					)
+				);
 			}
 
 			JSONPortletResponseUtil.writeJSON(
@@ -99,9 +118,6 @@ public class GetAnalyticsReportsPanelMVCResourceCommand
 			if (_log.isInfoEnabled()) {
 				_log.info(exception, exception);
 			}
-
-			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-				locale, getClass());
 
 			JSONPortletResponseUtil.writeJSON(
 				resourceRequest, resourceResponse,
@@ -140,6 +156,98 @@ public class GetAnalyticsReportsPanelMVCResourceCommand
 		);
 	}
 
+	private Map<String, Object> _getContext(
+		boolean validAnalyticsConnection, boolean readsEnabled, long plid,
+		ResourceResponse resourceResponse, Locale locale,
+		ResourceBundle resourceBundle) {
+
+		return HashMapBuilder.<String, Object>put(
+			"defaultTimeRange",
+			() -> {
+				TimeSpan defaultTimeSpan = TimeSpan.of(
+					TimeSpan.defaultTimeSpanKey());
+
+				TimeRange defaultTimeRange = defaultTimeSpan.toTimeRange(0);
+
+				return HashMapBuilder.<String, Object>put(
+					"endDate",
+					DateTimeFormatter.ISO_DATE.format(
+						defaultTimeRange.getEndLocalDate())
+				).put(
+					"startDate",
+					DateTimeFormatter.ISO_DATE.format(
+						defaultTimeRange.getStartLocalDate())
+				).build();
+			}
+		).put(
+			"defaultTimeSpanKey", TimeSpan.defaultTimeSpanKey()
+		).put(
+			"endpoints",
+			HashMapBuilder.<String, Object>put(
+				"getAnalyticsReportsHistoricalReadsURL",
+				() -> {
+					ResourceURL resourceURL =
+						resourceResponse.createResourceURL();
+
+					resourceURL.setResourceID(
+						"/analytics_reports/get_historical_reads");
+
+					return resourceURL.toString();
+				}
+			).put(
+				"getAnalyticsReportsHistoricalViewsURL",
+				() -> {
+					ResourceURL resourceURL =
+						resourceResponse.createResourceURL();
+
+					resourceURL.setResourceID(
+						"/analytics_reports/get_historical_views");
+
+					return resourceURL.toString();
+				}
+			).put(
+				"getAnalyticsReportsTotalReadsURL",
+				() -> {
+					ResourceURL resourceURL =
+						resourceResponse.createResourceURL();
+
+					resourceURL.setResourceID(
+						"/analytics_reports/get_total_reads");
+
+					return resourceURL.toString();
+				}
+			).put(
+				"getAnalyticsReportsTotalViewsURL",
+				() -> {
+					ResourceURL resourceURL =
+						resourceResponse.createResourceURL();
+
+					resourceURL.setResourceID(
+						"/analytics_reports/get_total_views");
+
+					return resourceURL.toString();
+				}
+			).build()
+		).put(
+			"languageTag", locale.toLanguageTag()
+		).put(
+			"namespace",
+			_portal.getPortletNamespace(
+				AnalyticsReportsPortletKeys.ANALYTICS_REPORTS)
+		).put(
+			"page",
+			HashMapBuilder.<String, Object>put(
+				"plid", plid
+			).build()
+		).put(
+			"readsEnabled", readsEnabled
+		).put(
+			"timeSpans", _getTimeSpansJSONArray(resourceBundle)
+		).put(
+			"validAnalyticsConnection", validAnalyticsConnection
+		).build();
+	}
+
 	private JSONObject _getPropsJSONObject(
 		AnalyticsReportsInfoItem<Object> analyticsReportsInfoItem,
 		Object analyticsReportsInfoItemObject, Locale locale) {
@@ -156,7 +264,32 @@ public class GetAnalyticsReportsPanelMVCResourceCommand
 			"title",
 			analyticsReportsInfoItem.getTitle(
 				analyticsReportsInfoItemObject, locale)
+		).put(
+			"trafficSources", JSONFactoryUtil.createJSONArray()
 		);
+	}
+
+	private JSONArray _getTimeSpansJSONArray(ResourceBundle resourceBundle) {
+		JSONArray timeSpansJSONArray = JSONFactoryUtil.createJSONArray();
+
+		Stream<TimeSpan> stream = Arrays.stream(TimeSpan.values());
+
+		stream.filter(
+			timeSpan -> timeSpan != TimeSpan.TODAY
+		).sorted(
+			Comparator.comparingInt(TimeSpan::getDays)
+		).forEach(
+			timeSpan -> timeSpansJSONArray.put(
+				JSONUtil.put(
+					"key", timeSpan.getKey()
+				).put(
+					"label",
+					ResourceBundleUtil.getString(
+						resourceBundle, timeSpan.getKey())
+				))
+		);
+
+		return timeSpansJSONArray;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
