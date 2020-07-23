66156abc58e10c124b673e54a867e65403f5fdaa
David Arques <david.arques@liferay.com>
2020-07-23 17:38:25 +0200

    LPS-117566 Add api to provide actions for the content dashboard items

---
+++ modules/apps/content-dashboard/content-dashboard-api/src/main/java/com/liferay/content/dashboard/item/action/ContentDashboardItemAction.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.item.action;
+
+/**
+ * @author David Arques
+ */
+public class ContentDashboardItemAction {
+
+	public ContentDashboardItemAction(String label, String name, String url) {
+		_label = label;
+		_name = name;
+		_url = url;
+	}
+
+	public String getLabel() {
+		return _label;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getUrl() {
+		return _url;
+	}
+
+	private final String _label;
+	private final String _name;
+	private final String _url;
+
+}
