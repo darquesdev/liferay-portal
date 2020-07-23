6aa7b5600f63240ec4f8959a1f62e916032e5f6e
Carlos Lancha <carlos.lancha@liferay.com>
2020-07-23 04:34:00 +0200

    LPS-117114 Pass fetchUrl as data instead as href so a link is not rendered in the dropdown but a button

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/servlet/taglib/util/ContentDashboardDropdownItemsProvider.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/servlet/taglib/util/ContentDashboardDropdownItemsProvider.java
@@ -97,17 +97,6 @@ public class ContentDashboardDropdownItemsProvider {
 				return dropdownItem;
 			},
 			() -> {
-				DropdownItem dropdownItem = new DropdownItem();
-
-				dropdownItem.setData(
-					HashMapBuilder.<String, Object>put(
-						"action", "showInfo"
-					).put(
-						"className", contentDashboardItem.getClassName()
-					).put(
-						"classPK", contentDashboardItem.getClassPK()
-					).build());
-
 				ResourceURL resourceURL =
 					_liferayPortletResponse.createResourceURL();
 
@@ -121,7 +110,18 @@ public class ContentDashboardDropdownItemsProvider {
 					"classPK",
 					String.valueOf(contentDashboardItem.getClassPK()));
 
-				dropdownItem.setHref(String.valueOf(resourceURL));
+				DropdownItem dropdownItem = new DropdownItem();
+
+				dropdownItem.setData(
+					HashMapBuilder.<String, Object>put(
+						"action", "showInfo"
+					).put(
+						"className", contentDashboardItem.getClassName()
+					).put(
+						"classPK", contentDashboardItem.getClassPK()
+					).put(
+						"fetchURL", String.valueOf(resourceURL)
+					).build());
 
 				dropdownItem.setLabel(_language.get(locale, "info"));
 
