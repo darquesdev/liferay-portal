52e89f6fa024f8ab6cd1e645e61476b938b2be5f
David Arques <david.arques@liferay.com>
2020-07-27 19:26:34 +0200

    LPS-117566 ContentDashboardItemActionProvider for journal articles

---
+++ modules/dxp/apps/analytics/analytics-reports-journal-impl/src/main/java/com/liferay/content/dashboard/item/action/JournalArticleContentDashboardItemActionProvider.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.content.dashboard.item.action;
+
+import com.liferay.analytics.reports.info.action.ViewAnalyticsReportsPanelContentDashboardItemActionProvider;
+import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItem;
+import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItemTracker;
+import com.liferay.asset.display.page.util.AssetDisplayPageUtil;
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.info.display.contributor.InfoDisplayContributor;
+import com.liferay.info.display.contributor.InfoDisplayContributorTracker;
+import com.liferay.info.display.contributor.InfoDisplayObjectProvider;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.PortletURLFactory;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Arques
+ */
+@Component(
+	property = "model.class.name=com.liferay.journal.model.JournalArticle",
+	service = ContentDashboardItemActionProvider.class
+)
+public class JournalArticleContentDashboardItemActionProvider
+	implements ContentDashboardItemActionProvider<JournalArticle> {
+
+	@Override
+	public ContentDashboardItemAction getContentDashboardItemAction(
+			JournalArticle journalArticle,
+			HttpServletRequest httpServletRequest)
+		throws ContentDashboardItemActionProviderException {
+
+		try {
+			if (!_isShow(
+					JournalArticle.class.getName(),
+					journalArticle.getResourcePrimKey(), httpServletRequest)) {
+
+				return null;
+			}
+
+			return _viewAnalyticsReportsPanelContentDashboardItemActionProvider.
+				getContentDashboardItemAction(
+					JournalArticle.class.getName(),
+					journalArticle.getResourcePrimKey(), httpServletRequest);
+		}
+		catch (PortalException portalException) {
+			throw new ContentDashboardItemActionProviderException(
+				portalException);
+		}
+	}
+
+	@Override
+	public String getKey() {
+		Class<?> clazz = getClass();
+
+		return clazz.getName();
+	}
+
+	private boolean _hasEditPermission(
+			long classNameId, long classPK,
+			LayoutPageTemplateEntry layoutPageTemplateEntry,
+			ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		Layout layout = _layoutLocalService.fetchLayout(
+			layoutPageTemplateEntry.getPlid());
+
+		if (layout == null) {
+			return false;
+		}
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.
+				getAssetRendererFactoryByClassNameId(classNameId);
+
+		AssetRenderer<?> assetRenderer = null;
+
+		if (assetRendererFactory != null) {
+			assetRenderer = assetRendererFactory.getAssetRenderer(classPK);
+		}
+
+		if (((assetRenderer == null) ||
+			 !assetRenderer.hasEditPermission(
+				 themeDisplay.getPermissionChecker())) &&
+			!LayoutPermissionUtil.contains(
+				themeDisplay.getPermissionChecker(), layout,
+				ActionKeys.UPDATE)) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean _isShow(
+			String className, long classPK,
+			HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		InfoDisplayContributor<?> infoDisplayContributor =
+			_infoDisplayContributorTracker.getInfoDisplayContributor(className);
+
+		if (infoDisplayContributor == null) {
+			return false;
+		}
+
+		InfoDisplayObjectProvider<?> infoDisplayObjectProvider =
+			infoDisplayContributor.getInfoDisplayObjectProvider(classPK);
+
+		if ((infoDisplayObjectProvider == null) ||
+			(infoDisplayObjectProvider.getDisplayObject() == null)) {
+
+			return false;
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			AssetDisplayPageUtil.getAssetDisplayPageLayoutPageTemplateEntry(
+				themeDisplay.getScopeGroupId(),
+				infoDisplayObjectProvider.getClassNameId(),
+				infoDisplayObjectProvider.getClassPK(),
+				infoDisplayObjectProvider.getClassTypeId());
+
+		if (layoutPageTemplateEntry == null) {
+			return false;
+		}
+
+		AnalyticsReportsInfoItem<?> analyticsReportsInfoItem =
+			_analyticsReportsInfoItemTracker.getAnalyticsReportsInfoItem(
+				_portal.getClassName(
+					infoDisplayObjectProvider.getClassNameId()));
+
+		if (analyticsReportsInfoItem == null) {
+			return false;
+		}
+
+		if (!_hasEditPermission(
+				infoDisplayObjectProvider.getClassNameId(),
+				infoDisplayObjectProvider.getClassPK(), layoutPageTemplateEntry,
+				themeDisplay)) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	@Reference
+	private AnalyticsReportsInfoItemTracker _analyticsReportsInfoItemTracker;
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private InfoDisplayContributorTracker _infoDisplayContributorTracker;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private PortletURLFactory _portletURLFactory;
+
+	@Reference
+	private ViewAnalyticsReportsPanelContentDashboardItemActionProvider
+		_viewAnalyticsReportsPanelContentDashboardItemActionProvider;
+
+}
